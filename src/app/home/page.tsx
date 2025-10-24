d1e7b9e22cd6c1032c5760b4437a1f94f9249643
victorfaruna <farunavictor043@gmail.com>
2025-10-24 11:50:22 +0100

    BNLB

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,15 +3,25 @@ import HeroCard from "@/components/HeroCard";
 import MainHeader from "@/components/MainHeader";
 import Tabs from "@/components/Tabs";
 import UpcomingMatches from "@/components/UpcomingMatches";
+import Image from "next/image";
 import React from "react";
 
 const HomePage = () => {
     return (
         <div className="w-full h-full relative">
             <MainHeader />
-            <div className="flex flex-col gap-5 px-[1rem]">
-                <HeroCard />
-                <UpcomingMatches />
+            <div className="flex flex-col gap-5 ">
+                <div className="flex flex-col gap-5 px-[1rem]">
+                    <HeroCard />
+                    <UpcomingMatches />
+                </div>
+                <Image
+                    src="/images/potw.webp"
+                    className="w-full"
+                    alt="banner"
+                    width={1000}
+                    height={1000}
+                />
             </div>
             <Tabs />
         </div>
