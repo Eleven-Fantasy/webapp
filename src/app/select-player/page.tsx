16f5358cce2bbaa033be3483abb6fc8f77b57cb3
victorfaruna <farunavictor043@gmail.com>
2025-11-08 14:21:24 +0100

    fix: suspense content

--- src/app/select-player/page.tsx
+++ src/app/select-player/page.tsx
@@ -1,16 +1,47 @@
 "use client";
 import PlayerHeader from "@/components/PlayerHeader";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
-const SelectPlayerPage = () => {
+const SelectPlayerContent = () => {
     const searchParams = useSearchParams();
     const matchId = searchParams.get("id");
     return (
-        <div className="w-full h-full relative pb-[4rem]">
+        <div className="w-full h-full pb-[4rem]">
             <PlayerHeader />
-            <div className="flex flex-col gap-5 px-[1rem] pb-[10rem]"></div>
+            <div className="flex flex-col gap-5 px-[1rem] pb-[10rem] oveflow-y-auto">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Player</th>
+                            <th>Rating</th>
+                            <th>Form</th>
+                            <th>Number</th>
+                            <th>Position</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Player</td>
+                            <td>Rating</td>
+                            <td>Form</td>
+                            <td>Number</td>
+                            <td>Position</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
+    );
+};
+
+const SelectPlayerPage = () => {
+    return (
+        <Suspense
+            fallback={<div className="w-full h-full pb-[4rem]">Loading...</div>}
+        >
+            <SelectPlayerContent />
+        </Suspense>
     );
 };
 
